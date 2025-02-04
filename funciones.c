df779d1087bbdca194a13f689326f1f3cc1ab7da
Peloponeso31 <tanilizquierdo@proton.me>
2025-02-04 17:19:58 -0600

    Participacion

--- funciones.c
+++ funciones.c
@@ -13,6 +13,7 @@ float velocidad(float distancia, float tiempo)
 // Funcion que calcula el indice de masa corporal.
 float imc(float peso, float altura)
 {
+  // Daniel participo.
   return peso/(altura * altura);
 }
 
@@ -26,11 +27,22 @@ void saludo(char * nombre, char * apellido)
 void clase_imc(float imc)
 {
   // Aqui algun desafortunado escribira su codigo.
+  // Alan participo.
+  if(imc<18.5){
+    printf("Tu imc es de bajo peso");
+  }else if(imc>=18.5&& imc<24.9){
+    printf("Tu imc es peso solidable");
+  }else if(imc >=25 && imc < 29.9){
+    printf("Tienes sobrepeso");
+  }else if(imc > 30){
+    printf("Tienes obesidad ");
+  }
 }
 
 // Entry point: Punto de entrada.
 int main(int argc, char * argv[])
 {
+  printf("%f", imc(92, 1.8));
   clase_imc(imc(92, 1.8));
   return 0;
 }
