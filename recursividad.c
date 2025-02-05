2465bc6cbec09d897a0a11fa0b31705e0ad329dc
Peloponeso31 <tanilizquierdo@proton.me>
2025-02-05 17:36:24 -0600

    Clase 5 feb

--- recursividad.c
+++ recursividad.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+int variable_global = 89;
+
 int factorial(int n)
 {
     // No recibe numeros negativos
@@ -12,11 +14,38 @@ int factorial(int n)
 // Si n vale 5, entonces 0 1 1 2 3.
 int fibonacci(int n)
 {
-    // Uy...
+    // Joshua participo.
+    //printf("Aqui hubo una llamada recursiva\n");
+    if (n < 2) return n;
+    return fibonacci (n - 1) + fibonacci(n - 2);
 }
+
+// Implementar una solucion recusiva al problema de palabras palindromas.
+// anitalavalatina = 1
+// Hola = 0
+// 101 = 1
+int palindromo(char * palabra)
+{
+    // Ya se la saben...
+}
+
+void funcion()
+{
+    printf("%i\n", variable_global);
+}
+
 
 int main(int argc, char * argv)
 {
-    printf("%i", factorial(5));
+    printf("%i\n", fibonacci(5));
+    funcion();
+
+    // Espacio de nombres | Namespace
+    int variable_interna = 50;
+    {
+        printf("%i\n", variable_global);
+        printf("%i\n", variable_interna);
+    }
+
     return 0;
 }
