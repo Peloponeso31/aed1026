065980afcebcf9b8062c664cbb726254f394c066
Peloponeso31 <tanilizquierdo@proton.me>
2025-02-07 17:55:10 -0600

    cambios apuntadores

--- apuntadores_funciones.c
+++ apuntadores_funciones.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
+/**
+ * @brief Funcion que estima la derivada de otra funcion con el metodo del cociente de diferencia simetrica.
+ * 
+ * @param f Funcion a derivar expresada en C.
+ * @param x punto de la funcion que se desea derivar.
+ * @param orden Orden de la derivada.
+ * @param h Cantidad pequeña necesaria para poder realizar la operacion de cociente de diferencia simetrica.
+ * @return Aproximacion de la derivada dada f(x): (f^[orden](x)). 
+ */
+double derivar(double (* f)(double), double x, int orden, double h){
+    if (orden > 1) {
+        return (derivar(f, x+h, orden-1, h) - derivar(f, x-h, orden-1, h)) / (2.0*h);
+    }
+    return (f(x+h) - f(x-h)) / (2.0*h);
+}
+
 void func(int * numero)
 {
     *numero = 42;
@@ -16,7 +32,12 @@ void iterar(void (*callback)(int), int * lista, int len)
     printf("\n");
 }
 
-void cuadrado(int n)
+double cuadrado(double n)
+{
+    return n*n;
+}
+
+void cuadrado_int(int n)
 {
     printf("%i, ", n*n);
 }
@@ -37,7 +58,10 @@ int main(int argc, char * argv[])
 
     // Se le pasa la direccion de memoria de las funciones como argumento.
     // Los callbacks permiten la reutilizacion de codigo.
-    iterar(&cuadrado, lista, 5);
+    iterar(&cuadrado_int, lista, 5);
     iterar(&cubo, lista, 5);
+
+    double resultado = derivar(&cuadrado, 4, 1, 0.001);
+    printf("%f", resultado);
     return 0;
 }
